b2eb720b761c24e6a550e64814de120d538a9ee9
bnandikanti <bnandikanti@yahoo.com>
2019-05-03 19:43:26 -0500

    finished file

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,7 +2,7 @@ import os
 import csv
 date = []
 totalValues = []
-Changes = []
+changes = []
 budget_csv_path = os.path.join("..", "Resources", "budget_data.csv")
 
 with open(budget_csv_path, newline="") as csvfile:
@@ -11,42 +11,48 @@ with open(budget_csv_path, newline="") as csvfile:
     csv_header = next(csv_reader)
 
     # Read through each row of data after the header and increase the count to get the total number of rows
-    print("Financial Analysis \n")
-    print("--------------------------------- \n")
+    # Get the count for total months, date and total the amount of all the values and place these in 3 lists
     count = 0
     total = 0
     for row in csv_reader:
         count = count + 1
         date.append(row[0])
         totalValues.append(row[1])
-    print("Total Months : " + str(count))
     for itemValue in totalValues:
         total = total + int(itemValue)
-    print("Total: $"+str(total))
     
-    averagechange = 0
-    for index in range(len(totalValues)- 1):
-        Change = int(totalValues[index + 1]) - int(totalValues[index])
-        Changes.append(Change)
-    print("Average Change: " + str(round(sum(Changes)/85, 2)))
-
-    zippedtuple = []
-    zippedtuple = zip(Changes, date)
-
-    for change, date in range(len(zippedtuple)):
-        if change == max(Changes):
-            print(max(Changes), date[date + 1])
-
-    print("Greatest Increase in Profits: ")
-
-
     
-    # Read through each row for column 2 except the header, and get the total amount of profit/losses
-    
-   # total = 0
-    #for row in csv_reader:
-        #totalValues.append(row[1])
-    
-    #print("Total: " + str(sum(totalValues)))
+#Calculate change and add it to a list
+averagechange = 0
+for index in range(len(totalValues)- 1):
+    change = int(totalValues[index + 1]) - int(totalValues[index])
+    changes.append(change)
 
-    #print(totalValues)
+
+# Get the greatest and least values for the change
+greatestIncrease = max(changes)
+greatestDecrease = min(changes)
+#Get the index for these values 
+greatestIncreaseIndex = changes.index(greatestIncrease)
+greatestDecreaseIndex = changes.index(greatestDecrease)
+
+#Use the index number from above to get the values from the date list
+print("Financial Analysis \n")
+print("--------------------------------- \n")
+print("Total Months : " + str(count))
+print("Total: $"+str(total))
+print("Average Change: $" + str(round(sum(changes)/85, 2)))
+print(f"Greatest Increase in Profits: {date[greatestIncreaseIndex + 1]}  (${max(changes)})")
+print(f"Greatest Decrease in Profits: {date[greatestDecreaseIndex + 1]}  (${min(changes)})")
+
+#Writing output to text file
+textfile = open('output/PyBank.txt', "w")
+textfile.write("Financial Analysis \n")
+textfile.write("---------------------------------\n")
+textfile.write("Total Months: " + str(count)+ "\n")
+textfile.write("Total: $" +str(total)+"\n")
+textfile.write("Average Change : $" +str(round(sum(changes)/85, 2))+"\n")
+textfile.write("Greatest Increase in Profits: " + str(date[greatestIncreaseIndex + 1])  + " ($" +str(max(changes)) + ")\n")
+textfile.write("Greatest Increase in Profits: " + str(date[greatestDecreaseIndex + 1])  + " ($" +str(min(changes)) + ")\n")
+     
+    
